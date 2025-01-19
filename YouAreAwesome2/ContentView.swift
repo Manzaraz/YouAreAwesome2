077f700f5a9ea6329e4ef4e093d023a375cc94fc
Christian Manzaraz <magnusmanz@icloud.com>
2025-01-18 21:36:52 -0300

    Initial Commit

---
+++ YouAreAwesome2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YouAreAwesome2
+//
+//  Created by Christian Manzaraz on 18/01/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
