096932dfd8ee6ae240161478c051b87f377d8d28
Christian Manzaraz <magnusmanz@icloud.com>
2025-01-19 00:28:47 -0300

    Updating Ch. 1.2: Adding a button

--- YouAreAwesome2/ContentView.swift
+++ YouAreAwesome2/ContentView.swift
@@ -8,17 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = "I Am A Programmer!"
+    
     var body: some View {
+        
         VStack {
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
             
-            Text("You Are Awesome!")
+            Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+            
+            Button("Click Me!") {
+                message = "I am an App Developer!"
+            }
+            .buttonStyle(.borderedProminent)
+            
         }
         .padding()
     }
