e924e96cb2f620947da060c214fa55991d59d0c4
Christian Manzaraz <magnusmanz@icloud.com>
2025-01-21 17:12:40 -0300

    Ch. 1.10 .minimumScaleFactor and the SwiftUI animation modifier (2025)

--- YouAreAwesome2/ContentView.swift
+++ YouAreAwesome2/ContentView.swift
@@ -16,29 +16,31 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-            Spacer()
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+//                .lineLimit(4)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: imageName)
             
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-            
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .multilineTextAlignment(.center)
-                .foregroundStyle(.red)
-                
+                .animation(.default, value: imageName)
             
             Spacer()
             
             Button("Press Me!") {
-                let messages = ["You Are Awesome!", "You Are Great!", "Fabulous? That's You!", "Swift Rules!", "When the Genius Needs Help, They Call You!"]
-                                
+                let messages = ["Gadzooks my friend! I am astonished at how utterly magnificient you are!", "You Are Awesome!", "You Are Great!", "Fabulous? That's You!", "Swift Rules!", "When the Genius Needs Help, They Call You!"]
+                
                 message = messages[messageIndex]
                 messageIndex = messageIndex == messages.count-1 ? 0 : messageIndex+1
-
+                
                 //                var randomImage = "image\(Int.random(in: 0..<10))" // MARK: this is my resolution
                 //                imageName = randomImage
                 
